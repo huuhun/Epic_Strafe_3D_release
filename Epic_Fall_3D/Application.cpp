55af0d6a0f7b96e042c4bddd672f163e45b04771
huuhun <letranhuuhung@gmail.com>
2024-02-29 16:03:13 +0700

    understanding the coord system

--- Epic_Fall_3D/Application.cpp
+++ Epic_Fall_3D/Application.cpp
@@ -21,7 +21,7 @@
 int main(int argc, char* args[]) {
 
 	if( !Window::initSDL() ) return -1;
-	SDL_Window* window{ Window::createGLWindow("Epic Fall 3D", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WindowSettings::SCREEN_WIDTH, WindowSettings::SCREEN_HEIGHT, SDL_WINDOW_SHOWN) };
+	SDL_Window* window{ Window::createGLWindow("Epic Fall 3D", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WindowSettings::SCR_WIDTH, WindowSettings::SCR_HEIGHT, SDL_WINDOW_SHOWN) };
 	SDL_GLContext context{ Window::createGLContext(window) };
 	Window::loadGLFunctionPointers();
 	Window::setGLVersion(4);
@@ -86,8 +86,23 @@ int main(int argc, char* args[]) {
 			if( e.type == SDL_QUIT )
 				goto cleanup;
 		}
-
-		Transform transform(0.001);
+		shader.Use();
+		// create transformations
+		//Transform transform(0.001);
+		glm::mat4 model      = glm::mat4(1.0f); // make sure to initialize matrix to identity matrix first
+		glm::mat4 view       = glm::mat4(1.0f);
+		glm::mat4 projection = glm::mat4(1.0f);
+		model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0, 0.0f, 0.0f));// converting -55 degrees to radians, model matrix is rotated around the x-axis by -55 degrees 
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); //The view matrix is translated along the z-axis by -3 units using 
+		projection = glm::perspective(glm::radians(45.0f), (float)WindowSettings::SCR_WIDTH / (float)WindowSettings::SCR_HEIGHT, 0.1f, 100.0f); //// Parameters: Field of view, aspect ratio, near clipping plane, far clipping plane
+		// retrieve the matrix uniform locations
+		unsigned int modelLoc = glGetUniformLocation(shader.getID(), "model");
+		unsigned int viewLoc = glGetUniformLocation(shader.getID(), "view");
+		// pass them to the shaders (3 different ways)
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[ 0 ][ 0 ]);
+		// note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+		shader.setMat4("projection", projection);
 
 		//Render here
 		renderer.Clear();
@@ -97,10 +112,10 @@ int main(int argc, char* args[]) {
 		faceTexture.Bind();
 
 		// get matrix's uniform location and set matrix
-		shader.Use();
+		//shader.Use();
 		//unsigned int transformLoc = glGetUniformLocation(shader.getID(), "transform");
 		//glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
-		shader.setMat4("transform", transform.getTransform());
+		//shader.setMat4("transform", transform.getTransform());
 		//renderer.DrawArrays();
 
 		//Render container
--- Epic_Fall_3D/utils/Settings.h
+++ Epic_Fall_3D/utils/Settings.h
@@ -1,6 +1,6 @@
 #pragma once
 namespace WindowSettings {
-	const int SCREEN_WIDTH  = 800;
-	const int SCREEN_HEIGHT = 600;
+	const int SCR_WIDTH  = 800;
+	const int SCR_HEIGHT = 600;
 	const int MAX_FPS = 60;
 }
