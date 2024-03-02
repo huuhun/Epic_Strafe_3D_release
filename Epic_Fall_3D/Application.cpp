dbdebd6a564e392e2b1993f1d9032b4e0cddc54e
huuhun <letranhuuhung@gmail.com>
2024-03-02 19:19:53 +0700

    setting up for the mouse

--- Epic_Fall_3D/Application.cpp
+++ Epic_Fall_3D/Application.cpp
@@ -22,6 +22,9 @@
 #include "Transform.h"
 #include "Model.h"
 
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
 float deltaTime = 0.0f; // time between current frame and last frame
 float lastFrame = 0.0f;
 
@@ -29,6 +32,13 @@ float lastFrame = 0.0f;
 glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+bool firstMouse = true;
+float yaw = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch = 0.0f;
+float lastX = 800.0f / 2.0;
+float lastY = 600.0 / 2.0;
+float fov = 45.0f;
 
 int main(int argc, char* args[]) {
 
@@ -42,7 +52,11 @@ int main(int argc, char* args[]) {
 	// configure global opengl state
 	// -----------------------------
 	glEnable(GL_DEPTH_TEST); // REMOVE THIS IF DRAW 2D ONLY
-
+	// tell GLFW to capture our mouse
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+	
 	//float vertices[] = {
 	//	// positions          // colors           // texture coords
 	//   0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
@@ -201,4 +215,52 @@ int main(int argc, char* args[]) {
 	Window::destroyWindow();
 
 	return 0;
+}
+
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+	float xpos = static_cast<float>( xposIn );
+	float ypos = static_cast<float>( yposIn );
+
+	if( firstMouse )
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouse = false;
+	}
+
+	float xoffset = xpos - lastX;
+	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+	lastX = xpos;
+	lastY = ypos;
+
+	float sensitivity = 0.1f; // change this value to your liking
+	xoffset *= sensitivity;
+	yoffset *= sensitivity;
+
+	yaw += xoffset;
+	pitch += yoffset;
+
+	// make sure that when pitch is out of bounds, screen doesn't get flipped
+	if( pitch > 89.0f )
+		pitch = 89.0f;
+	if( pitch < -89.0f )
+		pitch = -89.0f;
+
+	glm::vec3 front;
+	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front.y = sin(glm::radians(pitch));
+	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	cameraFront = glm::normalize(front);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	fov -= (float)yoffset;
+	if( fov < 1.0f )
+		fov = 1.0f;
+	if( fov > 45.0f )
+		fov = 45.0f;
 }
