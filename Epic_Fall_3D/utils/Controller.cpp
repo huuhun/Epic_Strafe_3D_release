39080d15538b2986b66dc3d43ed8b81e0880d438
fippinvn007 <fippinvn007@DESKTOP-MTIQ20V>
2024-03-02 17:02:07 +0700

    ok

--- Epic_Fall_3D/utils/Controller.cpp
+++ Epic_Fall_3D/utils/Controller.cpp
@@ -9,6 +9,7 @@ void processInput(GLFWwindow* window, float& deltaTime, glm::vec3& cameraPos,
     }
 
     float cameraSpeed = static_cast<float>( 2.5 * deltaTime );
+
     if( glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS )
         cameraPos += cameraSpeed * cameraFront;
     if( glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS )
