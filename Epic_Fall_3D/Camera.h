230a737d2ef04809b4b782b8d80493a1dc8011c6
huuhun <letranhuuhung@gmail.com>
2024-03-03 09:47:03 +0700

    ok

--- Epic_Fall_3D/Camera.h
+++ Epic_Fall_3D/Camera.h
@@ -1,7 +1,7 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
-//#include <glad/glad.h>
+//#include <glad/glad.h> //remove this line if "OpenGL header already included, remove this include, glad already provides "
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
